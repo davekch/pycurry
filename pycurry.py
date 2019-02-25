f03b9ebe5e2fa1033eb096051544092222294c97
davekch <dave-koch@web.de>
2019-02-25 23:53:24 +0100

    check types

--- pycurry.py
+++ pycurry.py
@@ -1,10 +1,19 @@
-from functools import wraps
-
 def curry(*types):
     def outerwrap(f):
         def innerwrap(*args):
+            # check if types are correct
+            for i in range(min(len(args),len(types))):
+                if types[i] is Any:
+                    continue
+                if not type(args[i]) is types[i]:
+                    raise TypeError("in {}: expected {}, got {}"\
+                        .format(f.__name__, types[i], type(args[i])))
+            # if too few arguments are given, return new curried function
             if len(types)>len(args):
                 return curry(*(types[len(args):]))(lambda *largs: f(*args, *largs))
             return f(*args)
         return innerwrap
     return outerwrap
+
+class Any:
+    pass
