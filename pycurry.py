07ffdfa598660f60988dd44bfeb62412df7043df
Adam Goldschmidt <adamgold7@gmail.com>
2019-03-07 12:31:31 +0200

    pass exception to curry function

--- pycurry.py
+++ pycurry.py
@@ -10,7 +10,7 @@ def curry(*types, exception=TypeError):
                         .format(f.__name__, types[i], type(args[i])))
             # if too few arguments are given, return new curried function
             if len(types)>len(args):
-                return curry(*(types[len(args):]))(lambda *largs: f(*args, *largs))
+                return curry(*(types[len(args):]), exception=exception)(lambda *largs: f(*args, *largs))
             return f(*args)
         return innerwrap
     return outerwrap
